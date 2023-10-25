2a260120fc11952c2680c5eb513c04aa1cd5ae68
Ponneeswari <148988025+Ponneeswari@users.noreply.github.com>
2023-10-25 11:47:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('hamburger');
+const navbar = document.queryselector('ul');
+
+hamburger.addeventlistener{'click', ()=>{
+   navbar.classlist.toggle('slide');
+}};
